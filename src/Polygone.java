44f4432a2f0f51663b799e5d8fb0035231fc5042
BALLAND Killian <killian.balland6@etu.univ-lorraine.fr>
2018-05-09 15:38:39 +0200

    Ajout du contenu de la classe Polygone

--- src/Polygone.java
+++ src/Polygone.java
@@ -1,4 +1,31 @@
+import java.awt.Graphics;
+import java.awt.Polygon;
 
 public abstract class Polygone extends FigureColoree {
-
+	
+	private Polygon p;
+	
+	public Polygone() {
+		super();
+		p = new Polygon();
+	}
+	
+	@Override
+	public void affiche(Graphics g) {
+		super.affiche(g);
+		g.drawPolygon(p);
+	}
+	
+	@Override
+	public int nbClics() {
+		return this.p.npoints;
+	}
+	
+	@Override
+	public void modifierPoints(Point[] points) {
+		this.p = new Polygon();
+		for(Point point : points) {
+			this.p.addPoint(point.rendreX(), point.rendreY());
+		}
+	}
 }
