6488fa8bf7883db7f6fb5c75069c1fdb3237ccc5
BALLAND Killian <killian.balland6@etu.univ-lorraine.fr>
2018-05-16 10:32:45 +0200

    Ajout de la classe Cercle pour ajouter un cercle au dessin

---
+++ src/Cercle.java
@@ -0,0 +1,43 @@
+import java.awt.Graphics;
+
+public class Cercle extends FigureColoree {
+	
+	@Override
+	public int nbPoints() {
+		return 2;
+	}
+
+	@Override
+	public int nbClics() {
+		return 2;
+	}
+
+	@Override
+	public void modifierPoints(Point[] points) {
+		if(points != null && points.length == this.nbPoints()) {
+			tab_mem[0] = points[0];
+			tab_mem[1] = points[1];
+		}
+	}
+
+	@Override
+	public boolean estDedans(int x, int y) {
+		if(tab_mem != null && tab_mem.length == this.nbClics()) {
+			Point centre = tab_mem[0];
+			double dist = centre.distance(tab_mem[1]);
+			return (x - centre.rendreX()) * (x - centre.rendreX()) + (y - centre.rendreY()) * (y - centre.rendreY()) <= (dist * dist);
+		}
+		return false;
+	}
+	
+	@Override
+	public void affiche(Graphics g) {
+		super.affiche(g);
+		if(tab_mem != null && tab_mem.length == this.nbPoints()) {
+			Point centre = tab_mem[0];
+			int dist = (int) Math.round(centre.distance(tab_mem[1]));
+			g.fillOval(centre.rendreX() - dist, centre.rendreY() - dist, dist, dist);
+		}
+	}
+
+}
