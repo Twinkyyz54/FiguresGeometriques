bf2bbc474a3e52635167758a971443f2aaadf2f5
Killian <Killian@Killian>
2018-05-29 00:15:57 +0200

    Transformation du losange ajouté

--- src/modele/Losange.java
+++ src/modele/Losange.java
@@ -28,4 +28,18 @@ public class Losange extends Quadrilatere {
 			}
 		}
 	}
+	
+	@Override
+	public void transformation(int dx, int dy, int idxcarre) {
+		if(tab_mem != null && idxcarre < tab_mem.length && idxcarre >= 0) {
+			Point p1 = tab_mem[0];
+			Point p2 = tab_mem[1];
+			if(p1 != null && p2 != null) {
+				Point modif = (idxcarre & 1) == 0 ? p1 : p2;
+				modif.incrementerX((idxcarre == 2) ? -dx : dx);
+				modif.incrementerY((idxcarre == 3) ? -dy : dy);
+				this.modifierPoints(new Point[] { p1, p2 });
+			}
+		}
+	}
 }
